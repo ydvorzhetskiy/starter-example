ae08e6e32a5f5a2e69305f46b185e870fa2cbf92
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-05-01 14:36:03 +0600

    Example webapp has been added

---
+++ web-app-example/src/main/java/com/martianpost/webappexample/WebAppExampleApplication.java
@@ -0,0 +1,13 @@
+package com.martianpost.webappexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppExampleApplication.class, args);
+    }
+
+}

b9aa9e858de1b7746a59e9d8e7524fdfe3c3d65d
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-05-01 11:38:35 +0600

    Example app has been added

---
+++ app-example/src/test/java/com/martianpost/appexample/AppExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martianpost.appexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
